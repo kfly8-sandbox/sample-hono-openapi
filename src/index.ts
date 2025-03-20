dbb84ba02a09cfad597680d92296d3918bba62c3
kobaken <kentafly88@gmail.com>
2025-03-20 14:40:39 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono'
 
-import { vValidator } from '@hono/valibot-validator';
+import { sValidator } from '@hono/standard-validator';
 
 import * as v from 'valibot'
+import { z } from 'zod'
+import { type } from 'arktype'
 
 const app = new Hono()
 
@@ -10,12 +12,33 @@ app.get('/', (c) => {
 	return c.text('Hello Hono!')
 })
 
-const _apiUserRequest = v.object({
-	limit: v.optional(v.number())
+// valibot
+const _apiUserRequestByValibot = v.object({
+	limit: v.optional(v.pipe(
+		v.string(),
+		v.transform((value) => {
+			const num = Number(value);
+			if (isNaN(num) || !isFinite(num)) {
+				return new Error('Invalid number');
+			}
+			return num;
+		}),
+		v.number(),
+	))
+});
+
+// zod
+const _apiUserRequestByZod = z.object({
+	limit: z.coerce.number().optional()
+});
+
+// arktype
+const _apiUserRequestByArktype = type({
+	limit: "string.integer.parse?",
 });
 
 app.get('/api/user',
-	vValidator('query', _apiUserRequest),
+	sValidator('query', _apiUserRequestByValibot),
 	(c) => {
 		const { limit } = c.req.valid('query');
 

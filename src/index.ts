40f603681ed5f75bf280dd924a3ae07230ffbc9b
kobaken <kentafly88@gmail.com>
2025-03-20 17:55:37 +0900

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from 'hono'
+import { describeRoute, openAPISpecs } from "hono-openapi";
+import { apiReference } from '@scalar/hono-api-reference'
 
-import { sValidator } from '@hono/standard-validator';
+//import { sValidator } from '@hono/standard-validator';
+//import { validator, resolver } from "hono-openapi/arktype";
+import { validator, resolver } from "hono-openapi/zod";
 
 import * as v from 'valibot'
 import { z } from 'zod'
@@ -8,9 +12,23 @@ import { type } from 'arktype'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-	return c.text('Hello Hono!')
-})
+// zod
+const _apiUserRequestByZod = z.object({
+	limit: z.coerce.number().optional()
+});
+
+const _apiUserResponseByZod = z.object({
+	limit: z.number(),
+});
+
+// arktype
+const _apiUserRequestByArktype = type({
+	limit: "string.integer.parse?",
+});
+
+const _apiUserResponseByArktype = type({
+	limit: "number",
+});
 
 // valibot
 const _apiUserRequestByValibot = v.object({
@@ -27,23 +45,54 @@ const _apiUserRequestByValibot = v.object({
 	))
 });
 
-// zod
-const _apiUserRequestByZod = z.object({
-	limit: z.coerce.number().optional()
-});
 
-// arktype
-const _apiUserRequestByArktype = type({
-	limit: "string.integer.parse?",
-});
+// Chose one of zod, arktype, valibot
+const _apiUserRequest = _apiUserRequestByZod
+const _apiUserResponse = _apiUserResponseByZod
 
-app.get('/api/user',
-	sValidator('query', _apiUserRequestByValibot),
+app.get(
+	'/test',
+	describeRoute({
+		description: "Say hello to the user",
+		responses: {
+		  200: {
+			description: "Successful greeting response",
+			content: {
+			  "application/json": {
+				schema: resolver(_apiUserResponse),
+			  },
+			},
+		  },
+		},
+		validateResponse: true,
+	}),
+	validator('query', _apiUserRequest),
 	(c) => {
 		const { limit } = c.req.valid('query');
-
 		return c.json({ limit });
 	}
+);
+
+app.get(
+	'/openapi',
+	openAPISpecs(app, {
+		documentation: {
+			openapi: '3.1.0',
+			info: {
+				title: 'Sample OpenAPI',
+				version: '0.0.1',
+				description: 'test openapi',
+			},
+		},
+	})
+);
+
+app.get(
+	'/docs/api-reference',
+	apiReference({
+		theme: 'saturn',
+		spec: { url: '/openapi' },
+	})
 );
 
 export default {
